ca78c4ded01f57d9265236a6cc659021681d7158
satinder99 <kainth00013@gmail.com>
2020-09-02 14:42:47 +0530

    apply registration page at front end

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,13 +3,37 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { RegisterComponent } from './register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
+//Angualr Material
+import * as Material from '@angular/material';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, RegisterComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    ReactiveFormsModule,
+    Material.MatToolbarModule,
+    Material.MatGridListModule,
+    Material.MatInputModule,
+    Material.MatFormFieldModule,
+    Material.MatDatepickerModule,
+    Material.MatNativeDateModule,
+    Material.MatIconModule,
+    Material.MatCheckboxModule
+  ],
+  exports : [
+    RegisterComponent,
+    Material.MatToolbarModule,
+    Material.MatGridListModule,
+    Material.MatInputModule,
+    Material.MatFormFieldModule,
+    Material.MatDatepickerModule,
+    Material.MatNativeDateModule,
+    Material.MatIconModule,
+    Material.MatCheckboxModule
   ]
 })
 export class HomeModule { }
