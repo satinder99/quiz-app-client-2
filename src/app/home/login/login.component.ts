16c4763ebc08d122450b3d1d7b9483f7bbb02ff9
satinder99 <kainth00013@gmail.com>
2020-09-08 20:58:23 +0530

    update register and login component and implement http requests

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -3,6 +3,9 @@ import {faFacebookSquare,faGooglePlusG,} from "@fortawesome/free-brands-svg-icon
 import { FormBuilder, Validators } from '@angular/forms';
 import { AuthService, GoogleLoginProvider, FacebookLoginProvider } from 'angularx-social-login';
 
+import {HomeService} from '../../services/home.service'
+import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -12,9 +15,12 @@ export class LoginComponent implements OnInit {
 
   google = faGooglePlusG;
   facebook = faFacebookSquare;
+
+  checker:Boolean;
   constructor(
     private fb:FormBuilder,
-    private authService : AuthService
+    private authService : AuthService,
+    private homeService : HomeService
   ) { }
 
   ngOnInit() {
@@ -26,26 +32,41 @@ export class LoginComponent implements OnInit {
   });
 
   onSubmit(){
-    console.log(this.loginForm)
+    /*var formData = new FormData();
+    formData.append('username',this.loginForm.get('email').value);
+    formData.append('password',this.loginForm.get('password').value);
+    */console.log(this.loginForm.get('email').value);
+    this.homeService.login({'username':this.loginForm.get('email').value,'password':this.loginForm.get('password').value}).subscribe(result => {
+      if(result.success){
+        Swal.fire({text:'Login successfull.'});
+      }
+      else{
+        Swal.fire({text : 'Login Failed'});
+      }
+    }),(err => {
+      Swal.fire({text : 'Something happened wrong!!!'});
+    })
 
   }
   logInWithGoogle(){
-    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).then(user => {
-      console.log('user fetched : ');
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).then(async user => {
+      await this.getUserDetail(user);
       console.log(user)
-      this.getUserDetail(user);
+      this.onSubmit();
     })
   }
-  logInWithFacebook(){
-    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID).then(user =>{
-      this.getUserDetail(user);
+  async logInWithFacebook(){
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID).then(async user =>{
+    await this.getUserDetail(user);
+      this.onSubmit();
     })
   }
 
   getUserDetail(user){
-    console.log('form values are : '+user);
-    console.log(user)
+    console.log(user.email)
+    console.log(user.id)
     this.loginForm.get('email').patchValue(user.email);
     this.loginForm.get('password').patchValue(user.id);
+    console.log(this.loginForm)
   }
 }
